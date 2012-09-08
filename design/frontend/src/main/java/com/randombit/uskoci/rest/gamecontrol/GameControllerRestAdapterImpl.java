e401e1b712568e0b5df6ced2b5bfe141c9a613a1
Vladimir Adami86 <vladimir.adamic@gmail.com>
2012-09-08 19:35:24 +0200

    Added new supported action - play card

--- design/frontend/src/main/java/com/randombit/uskoci/rest/gamecontrol/GameControllerRestAdapterImpl.java
+++ design/frontend/src/main/java/com/randombit/uskoci/rest/gamecontrol/GameControllerRestAdapterImpl.java
@@ -1,6 +1,7 @@
 package com.randombit.uskoci.rest.gamecontrol;
 
 import com.randombit.uskoci.card.model.Card;
+import com.randombit.uskoci.game.ActionNotAllowedException;
 import com.randombit.uskoci.game.GameController;
 import com.randombit.uskoci.game.dao.SingletonGameControllerDB;
 
@@ -13,43 +14,102 @@ import java.util.List;
 public class GameControllerRestAdapterImpl implements GameControllerRestAdapter {
     @Override
     public GameStatusResponse getResponse(GameStatusMessage message, String gameId) {
+        GameController gameController = getGameController();
+        GameStatusResponse gameResponse;
+        try {
+            gameResponse = makeMove(message, gameController);
+        } catch (ActionNotAllowedException e) {
+            e.printStackTrace();
+            gameResponse = new UnsupportedActionResponse();
+        }
+        return gameResponse;
+    }
 
-        // TODO: acces via factory
-
+    private GameController getGameController() {
+        // TODO: acces the real database DAO
         GameController gameController = SingletonGameControllerDB.instance.getAllControllers().get(0);
         if (!gameController.isGameStarted()) {
             gameController.startGame(4);
         }
+        return gameController;
+    }
 
-        String action = message.action;
+    private GameStatusResponse makeMove(GameStatusMessage message, GameController gameController) throws ActionNotAllowedException{
+        changeGameStateWithAction(gameController, message);
+        return buildGameResponse(message, gameController);
+    }
 
-/*
-        switch (action.toLowerCase()) {
-
-            case "drawcard":
-                gameController.drawCard(Integer.valueOf(message.userId));
-                break;
-        }
-        */
-
-        if ("drawcard".equals(action.toLowerCase())) {
-            gameController.drawCard(Integer.valueOf(message.userId));
-        }
-
+    private GameStatusResponse buildGameResponse(GameStatusMessage message, GameController gameController) {
         GameStatusResponse gameResponse = new GameStatusResponse();
         gameResponse.currentPlayerId = String.valueOf(gameController.getCurrentPlayerId());
 
+        gameResponse.playersCards = getPlayersCardIds(message, gameController);
+        gameResponse.beginningCardDrawn = gameController.getBeginningCardDrawn();
+        gameResponse.gameStarted = gameController.isGameStarted();
+        gameResponse.numberOfPlayersJoined = gameController.getNumberOfPlayersJoined();
+        gameResponse.currentPhase = gameController.getCurrentPhase();
+        gameResponse.discardedCards = getDiscardedCardIds(gameController);
+        gameResponse.player1Resources = getPlayersResources(1, gameController);
+        gameResponse.player2Resources = getPlayersResources(2, gameController);
+        gameResponse.player3Resources = getPlayersResources(3, gameController);
+        gameResponse.player4Resources = getPlayersResources(4, gameController);
+        gameResponse.playersPoints = getPlayersPoints(gameController);
+        return gameResponse;
+    }
+
+    private List<String> getPlayersPoints(GameController gameController) {
+        List<String> playerPoints = new ArrayList<String>();
+
+        for (int playerId=1; playerId<4; playerId++) {
+            playerPoints.add(String.valueOf(gameController.getPlayersPoints(playerId)));
+        }
+
+        return playerPoints;
+    }
+
+    private List<String> getPlayersResources(int playerId, GameController gameController) {
+        List<String> playerResourceIds = new ArrayList<String>();
+
+        for (Card cardInResources : gameController.getResources(playerId)) {
+            playerResourceIds.add(cardInResources.getId());
+        }
+        return playerResourceIds;
+    }
+
+    private List<String> getPlayersCardIds(GameStatusMessage message, GameController gameController) {
         List<String> playerCardIds = new ArrayList<String>();
 
         for (Card cardInHand : gameController.getPlayerCards(Integer.valueOf(message.userId))) {
             playerCardIds.add(cardInHand.getId());
         }
-        gameResponse.playersCards = playerCardIds;
-        gameResponse.beginningCardDrawn = gameController.getBeginningCardDrawn();
-        gameResponse.gameStarted = gameController.isGameStarted();
-        gameResponse.numberOfPlayersJoined = gameController.getNumberOfPlayersJoined();
-        gameResponse.currentPhase = gameController.getCurrentPhase();
+        return playerCardIds;
+    }
 
-        return gameResponse;
+    private List<String> getDiscardedCardIds(GameController gameController) {
+        List<String> discardedCards = new ArrayList<String>();
+        for (Card card: gameController.getDiscardPile())
+        {
+            discardedCards.add(card.getId());
+        }
+        return discardedCards;
+    }
+
+    private void changeGameStateWithAction(GameController gameController, GameStatusMessage message) throws ActionNotAllowedException {
+        String action = message.action;
+
+        if ("drawcard".equals(action.toLowerCase()))
+            gameController.drawCard(Integer.valueOf(message.userId));
+
+        if ("setnextturn".equals(action.toLowerCase()))
+            gameController.setNextPlayersTurn(Integer.valueOf(message.userId));
+
+        /* TODO: discard card from hand
+        if ("discardfromhand".equals(action.toLowerCase()))
+            gameController.setNextPhase(Integer.valueOf(message))*/
+
+        if ("playcard".equals(action.toLowerCase()))
+            if ("".equals(message.userId) && "".equals(message.cardId)) {
+                gameController.playCard(Integer.valueOf(message.userId), Integer.valueOf(message.cardId));
+            }
     }
 }
